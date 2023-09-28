b951b13945d878fb04ff64aa713ba2f8923f0373
Auroob <breu-inc@Breu-incs-MacBook-Pro.local>
2023-09-29 03:12:23 +0500

    Creating React

---
+++ dope-folio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './main.scss';
+import BackgroundImage from './components/backgroundImage';
+
+function App() {
+  return (
+    <div className="App">
+      <BackgroundImage />
+    </div>
+  );
+}
+
+export default App;
