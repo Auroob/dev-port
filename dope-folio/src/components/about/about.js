29058520ffbf29b58e69a8d050833d8374205c63
Auroob <breu-inc@Breu-incs-MacBook-Pro.local>
2023-09-30 02:08:49 +0500

    adjust about page css

--- dope-folio/src/components/about/about.js
+++ dope-folio/src/components/about/about.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "./about.css";
 
 import { aboutData } from "../../data/aboutData";
 
 export const About = () => {
     return (
         <div className="about">
-            <div className="content">
-                <div className="image">
+            <div className="about-content">
+                <div className="about-image">
                     <img src={aboutData.image} alt="Auroob" />
                 </div>
-                <h2>About Me</h2>
-                <p>{aboutData.content}</p>
+                <div className="about-text">
+                    <h2>About Me</h2>
+                    <p>{aboutData.content}</p>
+                </div>
             </div>
         </div>
     )
