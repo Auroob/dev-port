667cbb9a7cdc82e7ec3c83cfd3a0f85338e0043b
Auroob <breu-inc@Breu-incs-MacBook-Pro.local>
2023-09-30 08:26:58 +0500

    skills page

--- dope-folio/src/App.js
+++ dope-folio/src/App.js
@@ -3,12 +3,14 @@ import { About } from './components/about/about';
 //import BackgroundImage from './components/background/backgroundImage';
 import { Introduction } from './components/introduction/introduction';
 import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 
 function App() {
   return (
     <div className="App">
       <Introduction />
       <Projects />
+      <Skills />
       <About />
     </div>
   );
---
+++ dope-folio/src/components/skills/skills.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./skills.css";
+
+export const Skills = () => {
+    return (
+        <div className="skills">
+            <div className="skills-content">
+                <h2>My Technical Stack</h2>
+                <div className="skills-list">
+                    <img src="https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript" />
+                    <img src="https://img.shields.io/badge/TypeScript-3178C6.svg?style=for-the-badge&logo=typescript&logoColor=white" alt="TypeScript" />
+                    <img src="https://img.shields.io/badge/HTML5-E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" />
+                    <img src="https://img.shields.io/badge/CSS3-1572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="CSS3" />
+                    <img src="https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=react&logoColor=black" alt="React" />
+                    <img src="https://img.shields.io/badge/Angular-DD0031.svg?style=for-the-badge&logo=angular&logoColor=white" alt="Angular" />
+                    <img src="https://img.shields.io/badge/Svelte-FF3E00.svg?style=for-the-badge&logo=Svelte&logoColor=white" alt="Svelte" />
+                    <img src="https://img.shields.io/badge/Node.js-339933.svg?style=for-the-badge&logo=node.js&logoColor=white" alt="Node.js" />
+                    <img src="https://img.shields.io/badge/NestJS-E0234E.svg?style=for-the-badge&logo=NestJS&logoColor=white" alt="NestJS" />
+                    <img src="https://img.shields.io/badge/Express-000000.svg?style=for-the-badge&logo=express&logoColor=white" alt="ExpressJS" />
+                    <img src="https://img.shields.io/badge/PostgreSQL-4169E1.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="PostgreSQL" />                  
+                    <img src="https://img.shields.io/badge/Firebase-FFCA28.svg?style=for-the-badge&logo=Firebase&logoColor=black" alt="Firebase" />
+                    <img src="https://img.shields.io/badge/Docker-2496ED.svg?style=for-the-badge&logo=Docker&logoColor=white" alt="Docker" />
+                    <img src="https://img.shields.io/badge/Google%20Cloud-4285F4.svg?style=for-the-badge&logo=Google-Cloud&logoColor=white" alt="Google Cloud" />
+                    <img src="https://img.shields.io/badge/Terraform-844FBA.svg?style=for-the-badge&logo=Terraform&logoColor=white" alt="Terraform" />
+                    <img src="https://img.shields.io/badge/Helm-0F1689.svg?style=for-the-badge&logo=Helm&logoColor=white" alt="Helm" />
+                    <img src="https://img.shields.io/badge/Git-F05032.svg?style=for-the-badge&logo=Git&logoColor=white" alt="Git" />
+                    <img src="https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white" alt="GitHub" />
+                    <img src="https://img.shields.io/badge/Figma-F24E1E.svg?style=for-the-badge&logo=Figma&logoColor=white" alt="Figma" />
+                </div>
+            </div>
+        </div>
+    )
+}
