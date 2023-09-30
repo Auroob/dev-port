d99dfe5033b5b7cd488e4f12cea6947acdf29195
Auroob <breu-inc@Breu-incs-MacBook-Pro.local>
2023-09-30 05:28:03 +0500

    finalising intro page

--- dope-folio/src/components/introduction/introduction.js
+++ dope-folio/src/components/introduction/introduction.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./introduction.css";
+
 import { introductionData } from "../../data/introductionData";
 
 export const Introduction = () => {
     return (
         <div className="intro">
-            <div className="intro-description">
+            <div className="intro-content">
                 <h5>Hi There! 👋🏼 I’m</h5>
                 <h1>{introductionData.name}</h1>
                 <h5>Software Engineer</h5>
+                <div className="intro-social">
+                    <a href="https://github.com/Auroob" target="blank">
+                        <img src={introductionData.github} alt="Github" />
+                    </a>
+                    <a href="" target="blank">
+                        <img src={introductionData.gmail} alt="Gmail" height="2.25rem"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/auroob/" target="blank">
+                        <img src={introductionData.linkedin} alt="LinkedIn" />
+                    </a>
+                </div>
             </div>
         </div>
     );
--- dope-folio/src/data/introductionData.js
+++ dope-folio/src/data/introductionData.js
@@ -1,3 +1,10 @@
+import Github from "../images/links/github.png";
+import LinkedIn from "../images/links/linkedin.png";
+import Gmail from "../images/links/gmail.png";
+
 export const introductionData = {
-    name: "Auroob Ahmad"
+    name: "Auroob Ahmad",
+    github: Github,
+    linkedin: LinkedIn,
+    gmail: Gmail
 }
