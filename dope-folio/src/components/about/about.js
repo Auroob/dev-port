f1c480c27be95856e9e8e3c2aa946f21dafd7947
Auroob <breu-inc@Breu-incs-MacBook-Pro.local>
2023-09-30 09:36:37 +0500

    final design

--- dope-folio/src/components/about/about.js
+++ dope-folio/src/components/about/about.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./about.css";
 
+import Resume from "../../pdf/Auroob_Ahmad_Resume.pdf";
+
 import { aboutData } from "../../data/aboutData";
 
 export const About = () => {
     return (
         <div className="about">
             <div className="about-content">
-                <div className="about-image">
-                    <img src={aboutData.image} alt="Auroob" />
-                </div>
-                <div className="about-text">
-                    <h2>About Me</h2>
-                    <p style={{color: 'var(--text-color-white)'}}>{aboutData.content}</p>
+                <h2>About Me</h2>
+                <div className="about-description">
+                    <div className="about-image">
+                        <img src={aboutData.image} alt="Auroob" />
+                    </div>
+                    <div className="about-text">
+                        <p style={{color: 'var(--text-color-white)'}}>{aboutData.content}</p>
+                        <div className="about-button">
+                            <a href={Resume} download="Auroob Ahmad Resume">
+                                <button>
+                                    Download Resume
+                                </button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
